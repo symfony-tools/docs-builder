d2306f8dcd88223a7519c8f3955bc3dac3f9a7d8
Ryan Weaver <ryan@thatsquality.com>
2018-09-08 15:09:53 -0400

    improving test

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -3,8 +3,11 @@
 namespace SymfonyCasts\Tests;
 
 use Doctrine\RST\Builder;
+use Gajus\Dindent\Indenter;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use SymfonyDocs\HtmlKernel;
 
 class IntegrationTest extends TestCase
@@ -22,8 +25,22 @@ class IntegrationTest extends TestCase
             true // verbose
         );
 
-        $expectedSource = file_get_contents(__DIR__.'/fixtures/expected/datetime.html');
-        $actualSource = file_get_contents(__DIR__.'/_output/datetime.html');
-        $this->assertSame($expectedSource, $actualSource);
+        $finder = new Finder();
+        $finder->in(__DIR__.'/fixtures/expected/')
+            ->files()
+            ->depth('>=0')
+        ;
+
+        $indenter = new Indenter();
+        foreach ($finder as $expectedFile) {
+            $relativePath = $expectedFile->getRelativePathname();
+
+            $actualFilename = __DIR__.'/_output/'.$relativePath;
+            $this->assertFileExists($actualFilename);
+
+            $expectedSource = $indenter->indent($expectedFile->getContents());
+            $actualSource = $indenter->indent(file_get_contents($actualFilename));
+            $this->assertSame($expectedSource, $actualSource, sprintf('File %s is not equal', $relativePath));
+        }
     }
 }
