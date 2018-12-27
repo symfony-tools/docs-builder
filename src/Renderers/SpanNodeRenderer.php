2f9ed3e70597b026bba8187f795f53b7537e43cd
Nicolas PHILIPPE <nikophil@gmail.com>
2018-12-27 16:57:13 +0100

    test external url status code

--- src/Renderers/SpanNodeRenderer.php
+++ src/Renderers/SpanNodeRenderer.php
@@ -31,8 +31,24 @@ class SpanNodeRenderer extends BaseSpanNodeRenderer
     {
         $url = (string) $url;
 
+        if ($this->isExternalUrl($url)) {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_NOBODY, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+            curl_exec($ch);
+            $httpCode = (int) curl_getinfo($ch,  CURLINFO_RESPONSE_CODE);
+            curl_close($ch);
+
+            if ($httpCode < 200 || $httpCode >= 300) {
+                dump("$url : $httpCode");
+            }
+        }
+
         if (!$attributes) {
-            $attributes['class'] = sprintf('reference %s', 0 === strpos($url, 'http') ? 'external' : 'internal');
+            $attributes['class'] = sprintf('reference %s', $this->isExternalUrl($url) ? 'external' : 'internal');
         }
 
         return $this->templateRenderer->render(
@@ -43,5 +59,14 @@ class SpanNodeRenderer extends BaseSpanNodeRenderer
                 'attributes' => $attributes,
             ]
         );
+    }
+
+    private function isExternalUrl($url): bool
+    {
+        if (0 === strpos($url, 'http://') || 0 === strpos($url, 'https://')) {
+            return true;
+        }
+
+        return false;
     }
 }
