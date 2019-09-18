8f35febfba46f93912546ba0a53ab43d1b2cd30f
Nicolas PHILIPPE <nikophil@gmail.com>
2019-09-18 17:36:45 +0200

    Split command into several functions

--- src/Command/GithubReleaseCommand.php
+++ src/Command/GithubReleaseCommand.php
@@ -3,13 +3,16 @@
 namespace SymfonyDocsBuilder\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GithubReleaseCommand extends Command
 {
@@ -17,6 +20,12 @@ class GithubReleaseCommand extends Command
 
     /** @var SymfonyStyle */
     private $io;
+    /** @var HttpClientInterface */
+    private $client;
+
+    private $tag;
+    private $name;
+    private $description;
 
     public function __construct()
     {
@@ -28,21 +37,44 @@ class GithubReleaseCommand extends Command
         $this->setDescription('Create a release on github with a .phar as attachment.');
         // todo valid with regex
         $this->addArgument('tag', InputArgument::REQUIRED, 'Release\'s tag.');
-        $this->addArgument('name', InputArgument::REQUIRED, 'Release name');
-        $this->addArgument('description', InputArgument::REQUIRED, 'Release description');
+        $this->addArgument('name', InputArgument::OPTIONAL, 'Release name', 'Symfony docs builder %s');
+        $this->addArgument('description', InputArgument::OPTIONAL, 'Symfony docs builder %s');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
         $this->io = new SymfonyStyle($input, $output);
+        $this->client = $this->createHttpClient();
+
+        $tag = $input->getArgument('tag');
+        if (!preg_match('/v\d+\.\d+\.\d+/', $tag)) {
+            throw new RuntimeException(sprintf('"%s" is not a valid tag.', $tag));
+        }
+
+        $this->tag = $tag;
+        $this->name = sprintf($input->getArgument('name'), $tag);
+        $this->description = sprintf($input->getArgument('description'), $tag);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->compilePhar();
+
+        $this->addAssetToRelease($this->createRelease());
+    }
+
+    /**
+     * @throws ProcessFailedException
+     */
+    private function compilePhar(): void
+    {
         $process = Process::fromShellCommandline('./bin/compile', __DIR__.'/../..');
         $process->mustRun();
+    }
 
-        // TODO token form .env
+    private function createHttpClient(): HttpClientInterface
+    {
+// TODO token form .env
         $client = HttpClient::create(
             [
                 'headers' => [
@@ -51,25 +83,60 @@ class GithubReleaseCommand extends Command
             ]
         );
 
+        return $client;
+    }
+
+    private function createRelease(): int
+    {
         try {
-            $client->request(
+            $response = $this->client->request(
                 'POST',
+                // todo change repo
                 'https://api.github.com/repos/nikophil/test/releases',
                 [
                     'json' => [
-                        'tag_name' => $input->getArgument('tag'),
+                        'tag_name'         => $this->tag,
                         'target_commitish' => 'master',
-                        'name' => $input->getArgument('name'),
-                        'body' => $input->getArgument('description'),
-                        'draft' => false,
-                        'prerelease' => false
-                    ]
+                        'name'             => $this->name,
+                        'description'      => $this->description,
+                        'draft'            => false,
+                        'prerelease'       => false,
+                    ],
+                ]
+            );
+
+            return (int) $response->toArray()['id'];
+        } catch (ClientException $exception) {
+            throw new RuntimeException('Error while trying to create release. Maybe the tag name already exists?', 0, $exception);
+        }
+    }
+
+    private function addAssetToRelease(int $releaseId): void
+    {
+        try {
+            $this->client->request(
+                'POST',
+                sprintf('https://uploads.github.com/repos/nikophil/test/releases/%s/assets?name=docs.phar', $releaseId),
+                [
+                    'headers' => ['Content-Type' => 'application/octet-stream'],
+                    'body'    => file_get_contents(__DIR__.'/../../docs.phar'),
                 ]
             );
         } catch (ClientException $exception) {
-            $this->io->error('Error while trying to create release. Maybe the tag name already exists?');
+            $this->deleteRelease($releaseId);
+            throw new RuntimeException('Error while adding asset to release. Maybe the tag name already exists?', 0, $exception);
+        }
+    }
 
-            return;
+    private function deleteRelease(int $releaseId): void
+    {
+        try {
+            $this->client->request(
+                'DELETE',
+                sprintf('https://api.github.com/repos/nikophil/test/releases/%s', $releaseId)
+            );
+        } catch (ClientException $exception) {
+            throw new RuntimeException('Error while deleting release.', 0, $exception);
         }
     }
 }
