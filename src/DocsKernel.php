399e52809a18f47adea8827daeae2a203248c719
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-02-22 12:07:32 +0100

    Updated dependencies

--- src/DocsKernel.php
+++ src/DocsKernel.php
@@ -36,8 +36,6 @@ class DocsKernel extends Kernel
             $builder->getConfiguration()->getEventManager(),
             $builder->getErrorManager()
         );
-
-        $builder->setScannerFinder($this->buildContext->createFileFinder());
     }
 
     private function initializeListeners(EventManager $eventManager, ErrorManager $errorManager)
