fb8c2e866129c74626e0b607505f7b6ad13c2d3c
Wouter de Jong <wouter@wouterj.nl>
2023-12-27 17:09:40 +0100

    Unskip fixed test cases

--- tests/integration/HtmlIntegrationTest.php
+++ tests/integration/HtmlIntegrationTest.php
@@ -107,8 +107,8 @@ class HtmlIntegrationTest extends TestCase
     {
         $html = implode("\n", array_map('trim', explode("\n", $html)));
         $html = preg_replace('# +#', ' ', $html);
-        $html = preg_replace('# <#', '<', $html);
-        $html = preg_replace('#> #', '>', $html);
+        $html = preg_replace('#(?<!\w) <#', '<', $html);
+        $html = preg_replace('#> (?!\w)#', '>', $html);
         $html = preg_replace('#\R+#', "\n", $html);
 
         $html = substr($html, strpos($html, '<body>') + 6);
